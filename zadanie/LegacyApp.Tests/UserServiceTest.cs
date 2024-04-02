392990325440a654de16f35f95da7d5ae44caed0
Jonasz <s27202@pjwstk.edu.pl>
2024-04-02 19:03:34 +0200

    zadanie

---
+++ zadanie/LegacyApp.Tests/UserServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using JetBrains.Annotations;
+using LegacyApp;
+using Xunit;
+
+namespace LegacyApp.Tests;
+
+[TestSubject(typeof(UserService))]
+public class UserServiceTest
+{
+
+    [Fact]
+    public void METHOD()
+    {
+        
+    }
+}
